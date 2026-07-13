d42e4b48bf9df2d78ed6967e45889f0276b4350a
Suckscorpio999 <olinabin214@gmail.com>
2026-07-13 11:11:07 +0545

    Add InventoryRepository interface

---
+++ inventory-service/src/main/java/com/strivex/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.strivex.microservices.inventory.repository;
+
+import com.strivex.microservices.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityGreaterThanEqual(String skuCode, Integer quantity);
+}
