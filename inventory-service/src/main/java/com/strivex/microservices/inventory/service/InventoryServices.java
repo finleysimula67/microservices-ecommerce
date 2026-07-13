3b79527e7048ae4d108fa654d351d1b144db5567
Suckscorpio999 <olinabin214@gmail.com>
2026-07-13 11:11:41 +0545

    Add InventoryServices for stock checks

---
+++ inventory-service/src/main/java/com/strivex/microservices/inventory/service/InventoryServices.java
@@ -0,0 +1,15 @@
+package com.strivex.microservices.inventory.service;
+
+import com.strivex.microservices.inventory.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryServices {
+    private final InventoryRepository inventoryRepository;
+
+    public boolean isInStock(String skuCode, Integer quantity) {
+        return inventoryRepository.existsBySkuCodeAndQuantityGreaterThanEqual(skuCode, quantity);
+    }
+}
